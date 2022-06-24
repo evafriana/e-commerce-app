21db2dcca6c12037c01cab869c79801720a832b0
Eva Afriana <evafriana@Evas-MacBook-Air.local>
2022-06-24 14:29:25 +0200

    add responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,11 @@ import {
   Twitter,
 } from "@mui/icons-material";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   display: flex;
+  ${mobile({ flexDirection: "column" })}
 `;
 const Left = styled.div`
   display: flex;
@@ -39,6 +41,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ display: "none" })}
 `;
 const Title = styled.h3`
   margin-bottom: 30px;
@@ -57,6 +60,7 @@ const ListItem = styled.li`
 const Right = styled.div`
   flex: 1;
   padding: 20px;
+  ${mobile({ backgroundColor: "#fff8f8" })}
 `;
 const ContactItem = styled.div`
   margin-bottom: 20px;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,8 +2,9 @@ import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import NewsLatter from "../components/Newsletter";
+import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 const Title = styled.h1`
@@ -15,15 +16,18 @@ const FilterContainer = styled.div`
 `;
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({ margin: "0 20px", display: "flex", flexDirection: "column" })}
 `;
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({ marginRight: "0" })}
 `;
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
+  ${mobile({ margin: "10px 0" })}
 `;
 const Option = styled.option``;
 
@@ -68,7 +72,7 @@ export default function ProductList() {
         </Filter>
       </FilterContainer>
       <Products />
-      <NewsLatter />
+      <Newsletter />
       <Footer />
     </Container>
   );
